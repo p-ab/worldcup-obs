4be8cccd508210095840a89107e61063dba6fd9b
whoneedsthedevil <pavel.abramov.spb@gmail.com>
2018-07-03 17:49:26 +0300

    init app

---
+++ configs/mysql.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'localhost',
+  user: 'guest',
+  database: 'digital_war',
+  password: 'Trymydick2000',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+}
